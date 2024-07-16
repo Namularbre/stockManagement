68c61a8bcb71331e7c97fba346083d6198be4ea1
Thomas <namulabre@gmail.com>
2024-07-17 01:39:43 +0200

    Added formtypes for storage and product entities

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,23 +6,56 @@ use App\Entity\Product;
 use App\Entity\Storage;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('quantity')
-            ->add('createdAt', null, [
+            ->add('name', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'The name must have more than 3 chararters',
+                        'max' => 254,
+                        'maxMessage' => 'The name must be shorter than 254 characters'
+                    ]),
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => false,
+            ])
+            ->add('quantity', NumberType::class, [
+                'attr' => ['class' => 'form-control', 'min' => 0],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => false,
+            ])
+            ->add('createdAt', DateType::class, [
                 'widget' => 'single_text',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+                'required' => false,
             ])
             ->add('storage', EntityType::class, [
                 'class' => Storage::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'attr' => ['class' => 'form-select'],
+                'label_attr' => ['class' => 'form-label'],
             ])
         ;
     }
--- src/Form/StorageType.php
+++ src/Form/StorageType.php
@@ -4,15 +4,32 @@ namespace App\Form;
 
 use App\Entity\Storage;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class StorageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'attr' => ['class', 'form-control'],
+                'label_attr' => ['class', 'form-label'],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'The name must have more than 3 chararters',
+                        'max' => 254,
+                        'maxMessage' => 'The name must be shorter than 254 characters'
+                    ]),
+                ],
+            ])
         ;
     }
 
