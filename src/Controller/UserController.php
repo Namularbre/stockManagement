42c4b49c515a7d32f920b73eaa9a00d0662d731d
Thomas <namulabre@gmail.com>
2024-07-20 01:16:46 +0200

    Created a view to check the user profile

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user', methods: ['GET'])]
+    public function index(Security $security, UserRepository $userRepository): Response
+    {
+        $username = $security->getUser()->getUserIdentifier();
+        $user = $userRepository->findOneBy(['username' => $username]);
+
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
