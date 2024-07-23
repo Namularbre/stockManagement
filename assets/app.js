10a62e6be648f1f452008e72eb6a83c4558236a0
Thomas <namulabre@gmail.com>
2024-07-24 00:09:43 +0200

    Added bootstrap in the project

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,11 @@
 import './styles/app.css';
 import {Application} from "./vendor/@hotwired/stimulus/stimulus.index.js";
 import DeleteController from "./controllers/deleteController.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+// You can also import custom styles
+import './styles/app.scss';
 
 const application = Application.start();
 application.register("delete", DeleteController);
